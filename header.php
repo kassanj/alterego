8659d9c477238bce8896b24ae3247e7411e8fb75
kassanj <kassandram022@gmail.com>
2019-06-19 13:44:26 -0700

    style header

--- header.php
+++ header.php
@@ -24,35 +24,28 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'alterego' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$alterego_description = get_bloginfo( 'description', 'display' );
-			if ( $alterego_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $alterego_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+	<header id="masthead" class="site-header flex">
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'alterego' ); ?></button>
+		<nav id="site-navigation" class="main-navigation flex flex-auto items-end relative">
+
+			<a href="<?php echo get_site_url(); ?>" class="logo absolute top-0 left-0">
+				<img src="<?php echo get_template_directory_uri() . '/images/alter-ego-logo.svg'; ?>" class="db">
+			</a>
+
 			<?php
 			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
+				 'theme_location' => 'menu-1',
+				 'menu_id' => 'primary-menu',
+				 'menu_class' => 'main-menu ma0 pa0 list'
 			) );
 			?>
+
 		</nav><!-- #site-navigation -->
+
+		<div class="site-branding">
+			<?php echo wp_get_attachment_image(88, 'full'); ?>
+		</div><!-- .site-branding -->
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
