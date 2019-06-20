25a482c05b700bbc5222587e79cdcf90910f3d19
kassanj <kassandram022@gmail.com>
2019-06-20 12:55:20 -0700

    remove default features from archive product woocommerce page

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -268,3 +268,8 @@ remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add
 add_filter('single_product_archive_thumbnail_size', function( $size ) {
 	return 'full';
 } );
+
+remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 );
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_output_all_notices', 10 );
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
