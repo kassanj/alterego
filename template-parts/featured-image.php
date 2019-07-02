152c9a9811f3e4be8db450888f14a15ff8c13ca1
kassanj <kassandram022@gmail.com>
2019-07-02 09:48:12 -0700

    style search and basket component

--- template-parts/featured-image.php
+++ template-parts/featured-image.php
@@ -1,4 +1,5 @@
 <!-- Apply a width of 50% when the screen is 30em - 60em ( medium sized ) -->
 <div class="site-branding w-50-m">
+  <?php get_template_part('template-parts/header-search'); ?>
   <img src="<?php get_category_image('home'); ?>" class="db featured-image">
 </div><!-- .site-branding -->
